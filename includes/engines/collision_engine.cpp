a5025ea3feb6a547d5e988b49c2858be1d61ea55
Christopher Corona <35810446+owf684@users.noreply.github.com>
2024-01-28 15:28:31 -0600

    improved collisions

--- includes/engines/collision_engine.cpp
+++ includes/engines/collision_engine.cpp
@@ -36,6 +36,7 @@
 
 InputEngine input_ec;
 
+
 void CollisionEngine::update(std::unique_ptr<GameObject>& object, 
                              std::vector< std::unique_ptr<GameObject> >& render_buffer,
                              InputEngine& input_engine) 
@@ -65,8 +66,6 @@ void CollisionEngine::update(std::unique_ptr<GameObject>& object,
         
         ++i;
     }
-
-    handle_physics(object);
 }
 
 /* Function: down_collision()
@@ -78,21 +77,20 @@ void CollisionEngine::update(std::unique_ptr<GameObject>& object,
 
 bool CollisionEngine::down_collision(std::unique_ptr<GameObject>& current_object, std::unique_ptr<GameObject>& other)
 {
-    auto c_coll  = current_object->collider;
-    auto o_coll = other->collider;
-
-    
-    if ( (c_coll.bottom() > o_coll.top() && c_coll.top() < o_coll.top() )  && // check y 
-        (
-        (c_coll.right() > o_coll.left()    &&  c_coll.right() < o_coll.right())   ||
-        (c_coll.center_x() > o_coll.left() && c_coll.center_x() < o_coll.right()) || 
-        (c_coll.left() > o_coll.left() && c_coll.left() < o_coll.right()) 
-        ) 
-         
-    ) 
+    if (above(current_object, other))
     {
         current_object->collider.is.down = true;
-        if (!current_object->physics.pause) current_object->set_position( current_object->physics.position.x , other->physics.position.y - current_object->sprite.current->h+1);
+
+        if(!current_object->physics.pause)
+        {    
+            // calculate overlap and correct y position
+
+            float overlap = current_object->collider.bottom() - other->collider.top();
+            float corrected_y_position = current_object->physics.position.y - overlap;
+            current_object->set_position(current_object->physics.position.x, corrected_y_position);
+        }
+
+        y_inelastic_collision(current_object,other);
 
     }
 }
@@ -106,19 +104,19 @@ bool CollisionEngine::down_collision(std::unique_ptr<GameObject>& current_object
 
 bool CollisionEngine::up_collision(std::unique_ptr<GameObject>& current_object, std::unique_ptr<GameObject>& other)
 {
-    auto c_coll  = current_object->collider;
-    auto o_coll = other->collider;
-
-    if (c_coll.top() < o_coll.bottom() && 
-        c_coll.bottom() > o_coll.bottom())
+    if ( below(current_object,other) )
     {
-        if (c_coll.center_x() > o_coll.left() && c_coll.center_x() < o_coll.right() ||
-            c_coll.left() > o_coll.left() && c_coll.left() < o_coll.right() ||
-            c_coll.right() > o_coll.left() && c_coll.right() < o_coll.right() )
+        if (!current_object->physics.pause)
         {
-            current_object->collider.is.up = true;
+            // calculate overlap and correct y position
+            
+            float overlap = current_object->collider.top() - other->collider.bottom();
+            float corrected_y_position = current_object->physics.position.y - overlap;
+            current_object->set_position(current_object->physics.position.x, corrected_y_position);
+
         }
     }
+
 }
 
 /* Function: left_collision()
@@ -130,23 +128,20 @@ bool CollisionEngine::up_collision(std::unique_ptr<GameObject>& current_object, 
 
 bool CollisionEngine::left_collision(std::unique_ptr<GameObject>& current_object, std::unique_ptr<GameObject>& other)
 {       
-    auto c_coll  = current_object->collider;
-    auto o_coll = other->collider;
-
-    if (c_coll.left() <= o_coll.right() && 
-        c_coll.right() > o_coll.left() &&
-        c_coll.right() > o_coll.right() &&
-        !(c_coll.top() > o_coll.bottom() && c_coll.bottom() > o_coll.bottom() ) &&
-        !(c_coll.bottom() > o_coll.top() && c_coll.top() > o_coll.top() ) )
+    if (to_right(current_object,other))
     {
-        if (c_coll.center_y() > o_coll.top() && c_coll.center_y() < o_coll.bottom() ||
-            c_coll.top() > o_coll.top() && c_coll.top() < o_coll.bottom() ||
-            c_coll.bottom()-2 > o_coll.top() && c_coll.bottom() < o_coll.bottom()-2)
+
+        if (!current_object->physics.pause)
         {
-                
-        current_object->collider.is.left = true;
-       if (!current_object->physics.pause) current_object->set_position(o_coll.right(),c_coll.top());
+            // calculate overlap and correct x position
+            float overlap = current_object->collider.left()-other->collider.right();
+            float corrected_x_position = current_object->physics.position.x - overlap;
+            current_object->set_position(corrected_x_position,current_object->physics.position.y);
         }
+
+        current_object->collider.is.left = true;
+        x_inelastic_collision(current_object,other);
+        
     }
 
 }
@@ -160,24 +155,193 @@ bool CollisionEngine::left_collision(std::unique_ptr<GameObject>& current_object
 
 bool CollisionEngine::right_collision(std::unique_ptr<GameObject>& current_object, std::unique_ptr<GameObject>& other)
 {
-    auto c_coll  = current_object->collider;
-    auto o_coll = other->collider;
-
-    if (c_coll.right() >= o_coll.left() && 
-        c_coll.right() < o_coll.right() && 
-        c_coll.left() < o_coll.left() &&
-        !( c_coll.top() > o_coll.bottom() && c_coll.bottom() > o_coll.bottom() ))
+    if ( to_left(current_object,other) )
     {
-        if (c_coll.center_y() > o_coll.top() && c_coll.center_y() < o_coll.bottom() ||
-            c_coll.top() > o_coll.top() && c_coll.top() < o_coll.bottom() ||
-            c_coll.bottom() > o_coll.top() && c_coll.bottom() < o_coll.bottom() )
-        {
 
         current_object->collider.is.right = true;
-        if (!current_object->physics.pause) current_object->set_position(o_coll.left()-c_coll.box.w, c_coll.top());
+        
+        if (!current_object->physics.pause ) 
+        {
+            // calculate overlap and correct x position
+            float overlap = current_object->collider.right() - other->collider.left();
+            float corrected_x_position = current_object->physics.position.x - overlap;
+            current_object->set_position(corrected_x_position,current_object->physics.position.y);
+       
         }
+
+        x_inelastic_collision(current_object,other);
+
+        
     }
 
+}
+
+
+/* Funcion: above()
+*
+* Purpose: This function returns true
+* if object_1 is above object 2 and 
+* is overlapping it
+*
+*/
+
+bool CollisionEngine::above(std::unique_ptr<GameObject>& object_1, std::unique_ptr<GameObject>& object_2)
+{
+    int x_tolerance = 0;
+
+    // check the y axis
+
+    if (object_1->collider.bottom() > object_2->collider.top() &&
+        object_1->collider.top() < object_2->collider.top())
+    {
+        // check the x axis
+    
+        if (object_1->collider.center_x() > object_2->collider.left()+x_tolerance &&
+            object_1->collider.center_x() < object_2->collider.right()-x_tolerance)
+            return true;
+        else if (object_1->collider.left() > object_2->collider.left()+x_tolerance &&
+                 object_1->collider.left() < object_2->collider.right()-x_tolerance)
+            return true;
+        else if (object_1->collider.right() > object_2->collider.left()+x_tolerance &&
+                 object_1->collider.right() < object_2->collider.right()-x_tolerance)
+            return true;
+        else 
+            return false;
+    } else return false;
+}
+
+/* Function: below()
+*
+* Purpose: this function returns true
+* if object_1 is below object_2 and 
+* is overlapping it
+*
+*/
+
+bool CollisionEngine::below(std::unique_ptr<GameObject>& object_1, std::unique_ptr<GameObject>& object_2)
+{
+    int x_tolerance = 0;
+    // check the y xais
+    
+    if ( object_1->collider.top() < object_2->collider.bottom() &&
+         object_1->collider.top() > object_2->collider.top() &&
+         object_1->collider.bottom() > object_2->collider.bottom())
+    {
+
+        // check the x axis
+      
+        if (object_1->collider.left() > object_2->collider.left()+x_tolerance &&
+            object_1->collider.left() < object_2->collider.right()-x_tolerance)
+        {
+            return true;
+
+        } else if (object_1->collider.right() > object_2->collider.left()+x_tolerance &&
+                   object_1->collider.right() < object_2->collider.right()-x_tolerance)
+        {
+            return true;
+
+        } else if (object_1->collider.center_x() > object_2->collider.left()+x_tolerance &&
+                   object_1->collider.center_x() < object_2-> collider.right()-x_tolerance)
+        {
+            return true;
+
+        } else return false;
+
+
+    } else return false;
+}
+
+/* Function: to_left()
+*
+* Purpose: This function returns true
+* when object 1 is to left of object 2
+* and is overlapping it
+*
+*/
+bool CollisionEngine::to_left(std::unique_ptr<GameObject>& object_1, std::unique_ptr<GameObject>& object_2)
+{
+    // this tolerance prevents objects from falsely triggering right collisions when bottom
+    // sinks underneath a surface
+
+    int y_tolerance = 10;
+
+    // check x axis
+    if (object_1->collider.right() > object_2->collider.left() &&
+        object_1->collider.left() < object_2->collider.left() &&
+        object_1->collider.right() < object_2->collider.right() &&
+        object_1->collider.left() < object_2->collider.right() &&
+        object_1->collider.center_x() < object_2->collider.right() &&
+        object_1->collider.center_x() < object_2->collider.left() )
+        
+    {
+
+
+        // check y axis
+        if (object_1->collider.center_y() > object_2->collider.top() && 
+            object_1->collider.center_y() < object_2->collider.bottom())
+
+            return true;
+
+        else if ( object_1->collider.top() > object_2->collider.top() &&
+                  object_1->collider.top() < object_2->collider.bottom())
+
+            return true;
+
+        else if ( object_1->collider.bottom()-y_tolerance > object_2->collider.top() &&
+                  object_1->collider.bottom()-y_tolerance < object_2->collider.bottom())
+
+            return true;
+
+        else return false;
+
+
+    } else return false;
+
+}
+
+/* Function: to_right()
+*
+* Purpose: this function returns true
+* when object_1 is to the right of object_2
+* and is overlapping it
+*/
+
+bool CollisionEngine::to_right(std::unique_ptr<GameObject>& object_1, std::unique_ptr<GameObject>& object_2)
+{
+    // this tolerance prevents objects from falsely triggering right collisions when bottom
+    // sinks underneath a surface
+    int y_tolerance = 10;
+    
+    // check x axis
+
+    if (object_1->collider.left()  < object_2->collider.right() &&
+        object_1->collider.right() > object_2->collider.right() &&
+        object_1->collider.left() > object_2->collider.left() &&
+        object_1->collider.right() > object_2->collider.left() &&
+        object_1->collider.center_x() > object_2->collider.right() &&
+        object_1->collider.center_x() > object_2->collider.left())
+    {
+
+        // check y axis
+        if (object_1->collider.center_y() > object_2->collider.top() && 
+            object_1->collider.center_y() < object_2->collider.bottom())
+
+            return true;
+
+        else if ( object_1->collider.top() > object_2->collider.top() &&
+                  object_1->collider.top() < object_2->collider.bottom())
+
+            return true;
+
+        else if ( object_1->collider.bottom()-y_tolerance > object_2->collider.top() &&
+                  object_1->collider.bottom()-y_tolerance < object_2->collider.bottom())
+
+            return true;
+
+        else return false;
+
+
+    } else return false;
 }
 
 /* Function: handle_physics()
@@ -191,18 +355,7 @@ bool CollisionEngine::right_collision(std::unique_ptr<GameObject>& current_objec
 
 void CollisionEngine::handle_physics(std::unique_ptr<GameObject>& current_object)
 {
-    if ( (input_ec.arrow_keys.left && current_object->collider.is.left) ||
-         (input_ec.arrow_keys.right && current_object->collider.is.right))
-    {
-        current_object->physics.velocity.x = 0;
-        current_object->physics.acceleration.x = 0;
-    }
-
-    if (!input_ec.arrow_keys.up && current_object->collider.is.down)
-    {
-        current_object->physics.velocity.y = 0;
-        current_object->physics.acceleration.y = 0;
-    }
+    
 
     if (current_object->collider.is.up)
     {
@@ -211,4 +364,48 @@ void CollisionEngine::handle_physics(std::unique_ptr<GameObject>& current_object
     }
 
 }
-    
+
+/* Function: x_inelastic_collision()
+*
+* Purpose: This function calculates the 
+* inelastic collision between current_object
+* and other in the x_axis
+*
+*/  
+
+void CollisionEngine::x_inelastic_collision(std::unique_ptr<GameObject>& current_object, std::unique_ptr<GameObject>& other)
+{
+    // inelastic collisions
+    // V = (mass_a * velocity_a - mass_b * velocity_b) / (mass_a + mass_b)
+
+    float p = current_object->physics.momentum_x() - other->physics.momentum_x();
+    float m = current_object->physics.mass + other->physics.mass;
+    float v = p/m;
+
+
+    current_object->physics.velocity.x = v;
+    other->physics.velocity.x = v;
+
+}
+
+/* Function: y_inelastic_collision()
+*
+* Purpose: This function calculates the 
+* inelastic collision between current_object
+* and other in the y_axis
+*
+*/  
+
+void CollisionEngine::y_inelastic_collision(std::unique_ptr<GameObject>& current_object, std::unique_ptr<GameObject>& other)
+{
+    // inelastic collisions
+    // V = (mass_a * velocity_a - mass_b * velocity_b) / (mass_a + mass_b)
+
+    float p = current_object->physics.momentum_y() - other->physics.momentum_y();
+    float m = current_object->physics.mass + other->physics.mass;
+    float v = p/m;
+
+    current_object->physics.velocity.y = v;
+    other->physics.velocity.y = v;
+
+}
--- includes/engines/collision_engine.h
+++ includes/engines/collision_engine.h
@@ -17,12 +17,23 @@ class CollisionEngine{
 
 
     private:
+        // collision handlers
         bool down_collision(std::unique_ptr<GameObject>& current_object,std::unique_ptr<GameObject>& other);
         bool up_collision(std::unique_ptr<GameObject>& current_object, std::unique_ptr<GameObject>& other);
         bool left_collision(std::unique_ptr<GameObject>& current_object, std::unique_ptr<GameObject>& other);
         bool right_collision(std::unique_ptr<GameObject>& current_object, std::unique_ptr<GameObject>& other);
+        
+        // collision detection
+        bool above(std::unique_ptr<GameObject>& object_1, std::unique_ptr<GameObject>& object_2);
+        bool below(std::unique_ptr<GameObject>& object_1, std::unique_ptr<GameObject>& object_2);
+        bool to_left(std::unique_ptr<GameObject>& object_1, std::unique_ptr<GameObject>& object_2);
+        bool to_right(std::unique_ptr<GameObject>& object_1, std::unique_ptr<GameObject>& object_2);
 
+        // collision physics
         void handle_physics(std::unique_ptr<GameObject>& current_object);
+        void x_inelastic_collision(std::unique_ptr<GameObject>& current_object,std::unique_ptr<GameObject>& other);
+        void y_inelastic_collision(std::unique_ptr<GameObject>& current_object, std::unique_ptr<GameObject>& other);
+        void y_elastic_collision(std::unique_ptr<GameObject>& current_object, std::unique_ptr<GameObject>& other);
 
 };
 
