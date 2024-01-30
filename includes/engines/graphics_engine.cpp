ce75500c330b7eef19279ba1c106f991b1b96e75
Christopher Corona <35810446+owf684@users.noreply.github.com>
2024-01-30 14:34:35 -0600

    updated rendering code for SDL2 compatibility

--- includes/engines/graphics_engine.cpp
+++ includes/engines/graphics_engine.cpp
@@ -17,7 +17,6 @@
 #include "graphics_engine.h"
 
 
-
 // Constructor
 GraphicsEngine::GraphicsEngine(int input_width,int input_height, int input_bpp)
 {
@@ -27,22 +26,11 @@ GraphicsEngine::GraphicsEngine(int input_width,int input_height, int input_bpp)
 	bpp = input_bpp;
 	
 	
-	// initialize sdl
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		std::cerr << "Error Initializing SDL: " << SDL_GetError() << std::endl;
-	} 
-		
-	window = SDL_CreateWindow("Fuzzy C",
-							SDL_WINDOWPOS_CENTERED,
-							SDL_WINDOWPOS_CENTERED,
-							width, height,
-							SDL_WINDOW_SHOWN);
-
-	if (!window) {std::cerr << "Failed to Create Window\n";}
-
-	SDL_UpdateWindowSurface(window);
-
-    //SDL_WM_SetCaption("FuzzyC", NULL);
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_CreateWindowAndRenderer(width, height, 0, &window, &renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 
 }
 
@@ -53,19 +41,32 @@ GraphicsEngine::GraphicsEngine(int input_width,int input_height, int input_bpp)
 
 void GraphicsEngine::update()
 {
-	/*
-	SDL_FillRect(screen,0,0);
 
-	for(int i = 0; i < render_buffer.size(); i++)
-	{
-		SDL_BlitSurface(render_buffer[i]->sprite.current,NULL,screen,&render_buffer[i]->sprite.rect);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+	
+	for (int i = 0; i < render_buffer.size(); i++) {
+    
 
+
+
+    	// Convert the surface to a texture
+    	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, render_buffer[i]->sprite.current);
+
+    	// Check if the conversion was successful
+    	if (texture != nullptr) {
+
+        	// Copy the texture to the renderer
+       		SDL_RenderCopy(renderer, texture, nullptr, &render_buffer[i]->sprite.rect);
+
+        	// Free the texture (you can do this after rendering)
+        	SDL_DestroyTexture(texture);
+   	 	}
 	}
 
-	SDL_Flip(screen);
+	SDL_RenderPresent(renderer);
 
-	*/
-	SDL_UpdateWindowSurface(window);
+
 
 }
 
