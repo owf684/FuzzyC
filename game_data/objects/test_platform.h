950663d7071557bc2a4a3a7f97193dad59c8dbd9
Christopher Corona <35810446+owf684@users.noreply.github.com>
2024-04-04 22:35:03 -0500

    added generate_object function to each object

--- game_data/objects/test_platform.h
+++ game_data/objects/test_platform.h
@@ -8,6 +8,13 @@ class TestPlatform : public GameObject{
 public:
     void update() override;
     TestPlatform();
+    std::unique_ptr<GameObject> generate_object() 
+        {
+            std::unique_ptr<TestPlatform> object(new TestPlatform);
+
+            return  object;
+        }
+        
 };
 
 #endif
