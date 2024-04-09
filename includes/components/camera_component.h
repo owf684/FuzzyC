646258949909b15d0a29d37cf712187495567cb0
Christopher Corona <35810446+owf684@users.noreply.github.com>
2024-04-09 15:05:58 -0500

    added camera_component

---
+++ includes/components/camera_component.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef CAMERA_COMPONENT_H
+#define CAMERA_COMPONENT_H
+
+class CameraComponent {
+
+    public:
+
+        void update();
+        bool camera_active;
+
+
+};
+
+#endif
