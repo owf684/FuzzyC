9bf193b393b710241a556820134da8b90f7cf2bc
Christopher Corona <35810446+owf684@users.noreply.github.com>
2024-04-15 13:48:00 -0500

    changed argument name from mouse_position to position. too confusing to use mouse. need to update rdoh_util

--- includes/objects/rdoh.cpp
+++ includes/objects/rdoh.cpp
@@ -12,20 +12,22 @@ void RDOH::init_objects()
 } 
 
 
-void RDOH::generate_object(std::string object_name, ImVec2 mouse_position) {
+void RDOH::generate_object(std::string object_name, ImVec2 position) {
 
 	if (object_name == "bird") { 
 		bird anyObj = std::any_cast<bird>(object_library["bird"]); 
 		 graphics_engine.render_buffer.push_back(std::move(anyObj.generate_object())); 
-		 graphics_engine.render_buffer[graphics_engine.render_buffer.size()-1]->set_position(mouse_position.x,mouse_position.y);
+		 graphics_engine.render_buffer[graphics_engine.render_buffer.size()-1]->set_position(position.x,position.y);
+	
 	}
 
 	if (object_name == "test_platform") { 
 		test_platform anyObj = std::any_cast<test_platform>(object_library["test_platform"]); 
 		 graphics_engine.render_buffer.push_back(std::move(anyObj.generate_object())); 
-		 graphics_engine.render_buffer[graphics_engine.render_buffer.size()-1]->set_position(mouse_position.x,mouse_position.y);
+		 graphics_engine.render_buffer[graphics_engine.render_buffer.size()-1]->set_position(position.x,position.y);
+	
 	}
-
+	graphics_engine.render_buffer[graphics_engine.render_buffer.size()-1]->object_name = object_name;
 	auto object = std::move(graphics_engine.render_buffer.back());
 	graphics_engine.render_buffer.pop_back();
 	physics_engine.update(object);
