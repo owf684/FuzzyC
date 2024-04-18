69cdda7df4a964612d2cd42c9e338b6317971940
Christopher Corona <35810446+owf684@users.noreply.github.com>
2024-04-17 21:20:47 -0500

    updated camera component to have constructor. because camera_active was never initialized placing objects would sometimes have a false true. haha

---
+++ includes/components/camera_component.cpp
@@ -0,0 +1,14 @@
+#include "camera_component.h"
+
+
+
+CameraComponent::CameraComponent()
+{
+    camera_active = false;
+}
+
+
+void CameraComponent::update()
+{
+    
+}
--- includes/components/camera_component.h
+++ includes/components/camera_component.h
@@ -6,6 +6,7 @@ class CameraComponent {
 
     public:
 
+        CameraComponent();
         void update();
         bool camera_active;
 
