1bb415229fca3ef5322e66e592ebdc8dc3dd592e
Christopher Corona <35810446+owf684@users.noreply.github.com>
2024-05-09 21:50:15 -0500

    restructured game loop to accomade object scrolling of out of view objects. most stable version I've created so far

--- main.cpp
+++ main.cpp
@@ -7,23 +7,23 @@ using namespace std::chrono;
 int main(int argc, char* argv[])
 {
 
+// setup absolute paths
 exe_path = fs::canonical(argv[0]);
 exe_dir = exe_path.parent_path();
 engine_interface.update_scenes_vector();
 
-bool first_pass = true;
-
+// setup object handler
 object_handler.init_objects();
 
-// lib path
 object_util.set_object_lib_path( exe_dir.string() + "/includes/objects/object_library.qadon");
 object_util.set_rdoh_file_path( exe_dir.string() + "/includes/objects/rdoh.cpp");
 object_util.init(exe_dir.string());
 
+bool first_pass = true;
+
 // main game loop 
 while(!input_engine.quit)
 {	
-	// capture current time
 	auto dt_start = high_resolution_clock::now();	
 	
 	graphics_engine.update();
@@ -31,37 +31,28 @@ while(!input_engine.quit)
 
 	if (first_pass || play_pause)
 	{
-		// update all objects
+		// update all objects in render buffer
 		for(auto object_locator = graphics_engine.render_buffer.begin(); object_locator != graphics_engine.render_buffer.end(); object_locator++)
-		{	
+		{			
+			if (first_pass) first_pass = false;
 			
 			GameObject* object = object_locator->item;
+						
+			object->update();	
 
 			
-			if ( object != NULL)
-			{
+			if (graphics_engine.contain(object))
+				physics_engine.update(object);		
+			
+			collision_engine.update(object);
+			camera_engine.update(object);
+			scroll_engine.update(object);
+			sprite_engine.update(object);
 
-				if (graphics_engine.contain(object))
-				{
-					object->update();			
-					physics_engine.update(object);
-					std::list<GameObject*> other_objs;
-					if (object->collider.detect_collisions) graphics_engine.render_buffer.search(object, other_objs);
-					collision_engine.update(object,other_objs);
-
-				}
-					sprite_engine.update(object);
-					camera_engine.update(object);
-					scroll_engine.update(object);
-					graphics_engine.render_buffer.reinsert(object_locator);
-	
-			}
-			if (first_pass) first_pass = false;
+			graphics_engine.render_buffer.reinsert(object_locator);
+			
 		} 
-
 		
-
-
 	} else if (!play_pause)
 	{
 		scroll_engine.move_world();
