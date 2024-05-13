f29ba59c3495cf25cce86991069494dca8c3b3fb
Christopher Corona <35810446+owf684@users.noreply.github.com>
2024-05-13 13:10:19 -0500

    corrected dt calculation and removed graphicsEngine.update from the measure dt game loop. This was causing lots of problems with loading scenes. also seperated graphics_engine call to a thread. I belive this improved graphics response. Need to do more testing.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <thread>
 #undef main
 using namespace std::chrono;
 
@@ -6,7 +7,6 @@ using namespace std::chrono;
 
 int main(int argc, char* argv[])
 {
-
 // setup absolute paths
 exe_path = fs::canonical(argv[0]);
 exe_dir = exe_path.parent_path();
@@ -14,58 +14,63 @@ engine_interface.update_scenes_vector();
 
 // setup object handler
 object_handler.init_objects();
-
 object_util.set_object_lib_path( exe_dir.string() + "/includes/objects/object_library.qadon");
 object_util.set_rdoh_file_path( exe_dir.string() + "/includes/objects/rdoh.cpp");
 object_util.init(exe_dir.string());
 
-bool first_pass = true;
+// Initialize variables for timing
+typedef std::chrono::high_resolution_clock Clock;
+auto currentTime = Clock::now();
+auto endTime = Clock::now();
 
 // main game loop 
 while(!input_engine.quit)
 {	
-	auto dt_start = high_resolution_clock::now();	
-	
-	graphics_engine.update();
+
+	std::thread graphicsThread([&]() {
+        graphics_engine.update();
+    });
+	//graphics_engine.update();
+
 	input_engine.update();
 
-	if (first_pass || play_pause)
-	{
-		// update all objects in render buffer
-		for(auto object_locator = graphics_engine.render_buffer.begin(); object_locator != graphics_engine.render_buffer.end(); object_locator++)
-		{			
-			if (first_pass) first_pass = false;
-			
-			GameObject* object = object_locator->item;
-						
-			object->update();	
+    currentTime = Clock::now();
 
-			
+	if ( play_pause)
+	{
+		
+		for(auto object_locator = graphics_engine.render_buffer.begin(); object_locator != graphics_engine.render_buffer.end(); object_locator++)
+		{				
+			GameObject* object = object_locator->item;					
+			object->update();		
 			if (graphics_engine.contain(object))
-				physics_engine.update(object);		
-			
+				physics_engine.update(object);	
+		
 			collision_engine.update(object);
 			camera_engine.update(object);
 			scroll_engine.update(object);
 			sprite_engine.update(object);
-
-			graphics_engine.render_buffer.reinsert(object_locator);
-			
+			graphics_engine.render_buffer.reinsert(object_locator);		
 		} 
-		
+
 	} else if (!play_pause)
 	{
 		scroll_engine.move_world();
 	}
 
-	// update delta time step
-	auto frame_rate = (1.0 / frames_per_second_f)*micro_seconds_f;
+	endTime = Clock::now();
 
-	if (frame_rate - dt >0)
-		usleep(frame_rate - dt);
+	std::chrono::nanoseconds duration = endTime - currentTime;
+	long frame_rate_delay = ( (1.0/frames_per_second_f) - (duration.count() * 1e-9) ) * 1e9;
+	if (frame_rate_delay > 0){
+		std::this_thread::sleep_for(std::chrono::nanoseconds(frame_rate_delay));}
+	else	{
+		std::cout << "warning: frame rate delay is negative" << std::endl;}
 
-	auto dt_end = high_resolution_clock::now();
-	auto dt_final = duration_cast<milliseconds>(dt_end - dt_start);
-	dt = dt_final.count()/1000.0;
+	dt = 1 / frames_per_second_f;
+
+
+	graphicsThread.join();
+
 }
 }
